11e3e57034fee8bdd0c37024dbab84a91a6a0173
Cthulhu <69705320+MattD138@users.noreply.github.com>
2020-08-24 19:05:26 +1000

    User can change playlist name

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
+  
   constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
