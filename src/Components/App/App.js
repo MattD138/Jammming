f43c481602e77082631cb737ac84bd467367295e
Cthulhu <69705320+MattD138@users.noreply.github.com>
2020-08-24 18:23:36 +1000

    Defines basic layout and component structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './App.css';
+import { Playlist } from '../Playlist/Playlist';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { findAllInRenderedTree } from 'react-dom/test-utils';
+
+const hardcodedResults = [
+  {
+    name:   'Faded',
+    artist: 'ZHU',
+    album:  'THE NIGHTDAY',
+    id:     1
+  },
+  {
+    name:   'Sandstorm',
+    artist: 'Darude',
+    album:  'Before The Storm',
+    id:     2
+  },
+  {
+    name:   'Sanctuary',
+    artist: 'Gareth Emery',
+    album:  'Sanctuary',
+    id:     3
+  },
+  {
+    name:   'Shelter',
+    artist: 'Porter Robinson',
+    album:  'Shelter',
+    id:     4
+  },
+  {
+    name:   'Sun & Moon',
+    artist: 'Above & Beyond',
+    album:  'Group Therapy',
+    id:     5
+  }
+]
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: hardcodedResults
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
